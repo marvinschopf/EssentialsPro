8ab1764753c76c95fa17c57e1c66014ce8b48a8a
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-04-15 14:06:36 +0200

    Fixes

--- src/main/kotlin/de/marvnet/minecraft/essentialspro/commands/GamemodeCommand.kt
+++ src/main/kotlin/de/marvnet/minecraft/essentialspro/commands/GamemodeCommand.kt
@@ -15,28 +15,15 @@ class GamemodeCommand: CommandExecutor {
                 if(args.size == 1) {
                     if(sender is Player) {
                         val p: Player = sender as Player
-                        if(EssentialsPro.getAsync()) {
-                            Bukkit.getScheduler().runTaskAsynchronously(EssentialsPro.getPlugin(), {
-                                GamemodeUtil().setGamemode(args[0], p)
-                            })
-                        } else {
-                            GamemodeUtil().setGamemode(args[0], p)
-                        }
+                        GamemodeUtil().setGamemode(args[0], p)
                     } else {
                         sender.sendMessage(EssentialsPro.getOnlyPlayer())
                     }
                 } else if(args.size == 2) {
                     val p: Player = Bukkit.getServer().getPlayer(args[0])
                     if(p != null) {
-                        if(EssentialsPro.getAsync()) {
-                            Bukkit.getScheduler().runTaskAsynchronously(EssentialsPro.getPlugin(), {
-                                GamemodeUtil().setGamemode(args[1], p)
-                                sender.sendMessage("§6The gamemode of " + p.displayName + " has been changed to " + args[1])
-                            })
-                        } else {
-                            GamemodeUtil().setGamemode(args[1], p)
-                            sender.sendMessage("§6The gamemode of " + p.displayName + " has been changed to " + args[1])
-                        }
+                        GamemodeUtil().setGamemode(args[1], p)
+                        sender.sendMessage("§6The gamemode of " + p.displayName + " has been changed to " + args[1])
                     } else {
                         sender.sendMessage(EssentialsPro.getUnknownPlayer())
                     }
